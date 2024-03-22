8bc06ae7ab1f011c4236e52299c1efe6666fa348
Gustavo Henrique <160801791+Gugahl@users.noreply.github.com>
2024-03-22 16:57:38 -0300

    Add files via upload

---
+++ joao_gabriel/Exercícios - Aula 01 - Introdução à Python/Exercício 03.py
@@ -0,0 +1,16 @@
+"""Exercício 03"""
+"""
+Como calcular em Python a área do triângulo a seguir em
+Python?
+altura = 8
+diametro = 12
+base = 10
+E como eu poderia calcular a diferença entre a área deste
+triângulo e a de um outro cujas dimensões sejam o dobro das
+desse?
+"""
+
+diferença_entre_areas = 20 * 16 / 2 - 10 * 8 / 2
+print(f'A área do triângulo fornecido foi: {10 * 8 / 2}\n'
+    f'A área do segundo triângulo foi: {20 * 16 / 2}\n'
+    f'A diferença entre as duas áreas foi: {diferença_entre_areas}.')
