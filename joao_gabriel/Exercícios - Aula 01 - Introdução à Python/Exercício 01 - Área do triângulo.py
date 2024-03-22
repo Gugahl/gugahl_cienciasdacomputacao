f8e4bfe19ec1f19a113020758c21eea6f92a0b5e
Gustavo Henrique <160801791+Gugahl@users.noreply.github.com>
2024-03-22 17:22:47 -0300

    Add files via upload

---
+++ joao_gabriel/Exercícios - Aula 01 - Introdução à Python/Exercício 01 - Área do triângulo.py
@@ -0,0 +1,11 @@
+"""Exercício 01"""
+"""
+Como calcular em Python a área do triângulo a seguir em
+Python?
+altura = 8
+diametro = 12
+base = 10
+"""
+
+area_do_triangulo = (10 * 8)/2
+print(f'A área do triângulo fornecido foi: {area_do_triangulo}')
