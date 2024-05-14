a7068bfdc88d7f33837bac175a05a0beede74d8a
Gustavo Henrique <ustavo.santos@gmail.com>
2024-05-13 23:56:56 -0300

    listas e matrizes

--- joão_gabriel/Exercícios - Aula 07 - Listas e Matrizes/Exercício 07 - Entrada de usuário.py
+++ joão_gabriel/Exercícios - Aula 07 - Listas e Matrizes/Exercício 07 - Entrada de usuário.py
@@ -8,4 +8,28 @@ e colunas da matriz) e então alimentar os números de cada linha.
 exercicio = 'Exercício #07'
 print(f'-=' * 15 + f'\n' + f'{exercicio:^30}' + f'\n' + f'-=' * 15)
 
+linhas = int(input("Digite o número de linhas da matriz: "))
+colunas = int(input("Digite o número de colunas da matriz: "))
 
+matriz = []
+for l in range(linhas):
+    linha = []
+    for c in range(colunas):
+        linha.append(0)
+    matriz.append(linha)
+
+print(f'\nMatriz criada:')
+print(matriz, f'\n')
+
+soma_colunas = [0] * colunas
+for l in range(linhas):
+    for c in range(colunas):
+        matriz[l][c] = int(input(f"Digite um valor para [{l + 1}, {c + 1}]: "))
+        soma_colunas[c] += matriz[l][c]
+
+print(f'\nMatriz alimentada:')
+print(matriz)
+
+print("\nSoma de cada coluna:")
+for soma in soma_colunas:
+    print(soma)
--- joão_gabriel/Exercícios - Aula 07 - Listas e Matrizes/Exercício 08 - Ordenar uma matriz.py
+++ joão_gabriel/Exercícios - Aula 07 - Listas e Matrizes/Exercício 08 - Ordenar uma matriz.py
@@ -4,6 +4,60 @@ Escreva um programa que deve ordenar uma matriz (pré-estabelecida) na ordem
 ascendente de acordo com as somas de suas linhas.
 Ex: [[1, 2, 3], [2, 4, 5], [1, 1, 1]] → [[1, 1, 1], [1, 2, 3], [2, 4, 5]]
 """
-
+# incompleto
 exercicio = 'Exercício #08'
 print(f'-=' * 15 + f'\n' + f'{exercicio:^30}' + f'\n' + f'-=' * 15)
+
+matriz = [[1, 2, 3], [2, 4, 5], [1, 1, 1]]
+
+print(matriz)
+
+matriznova = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+maior = menor = meio = 0
+soma = [0, 0, 0]
+for l in range(0, 3):
+    for c in range(0, 3):
+        soma[l] += matriz[l][c]
+        if soma[l] == 0:
+            menor = soma[l]
+            meio = soma[l]
+            maior = soma[l]
+        elif soma[l] > maior:
+            maior = soma[l]
+        elif soma[l] < menor:
+            menor = soma[l]
+        else:
+            meio = soma[l]
+    print(f'Soma {l + 1}: ', soma[l], sep='', end=', ' if l < 2 else '.')
+print(f'Maior: {maior}, Menor: {menor}, Meio: {meio}')
+
+for l in range(0, 3):
+    if l == 0:
+        for l in range(0, 1):
+            for c in range(0, 3):
+                if c == 0:
+                    matriznova[l][c] = matriz[l][c]
+                elif c == 1:
+                    matriznova[l][c] = matriz[l][c]
+                else:
+                    matriznova[l][c] = matriz[l][c]    
+    elif l == 1:
+        for l in range(1, 2):
+            for c in range(0, 3):
+                if c == 0:
+                    matriznova[l][c] = matriz[l][c]
+                elif c == 1:
+                    matriznova[l][c] = matriz[l][c]
+                else:
+                    matriznova[l][c] = matriz[l][c]
+    elif l == 2:
+        for l in range(2, 3):
+            for c in range(0, 3):
+                if c == 0:
+                    matriznova[l][c] = matriz[l][c]
+                elif c == 1:
+                    matriznova[l][c] = matriz[l][c]
+                else:
+                    matriznova[l][c] = matriz[l][c]
+
+print(f'\n', matriznova, sep='')
