910abd93ca73a198c1bb6c25d1ebc39420813c5f
Gustavo Lima <160801791+Gugahl@users.noreply.github.com>
2024-07-06 20:50:48 -0300

    Delete primeiro_semestre/arquitetura_pcea_de_computadores directory

--- primeiro_semestre/arquitetura_pcea_de_computadores/Exercícios Python/Conversão Decimal-Binário.py
+++
@@ -1,3 +0,0 @@
-numero_escolhido = int(input("Digite um número: "))
-base_binaria = bin(numero_escolhido)[2:]
-print(f'O número {numero_escolhido} convertido para a base binária é igual a: {base_binaria}.')
--- primeiro_semestre/arquitetura_pcea_de_computadores/Exercícios Python/Soma entre dois valores.py
+++
@@ -1,7 +0,0 @@
-# Soma entre dois valores utilizando função, sem o uso de entrada do usuário.
-
-def soma(primeiro_valor, segundo_valor):
-    resultado = primeiro_valor + segundo_valor
-    return resultado
-
-print(soma(32, 12))
